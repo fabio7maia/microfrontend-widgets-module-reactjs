6985de938bda8c5ea383a798f403d183d1b7055b
Fábio Maia <fabio.maia@itsector.pt>
2021-09-23 21:59:29 +0100

    Removed env keys and .env file

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,3 +3,8 @@ export const LoggerConfig = {
 	error: process.env.REACT_APP_LOGGER_ERROR === 'false' ? false : true,
 	log: process.env.REACT_APP_LOGGER_LOG === 'false' ? false : true,
 };
+
+export const ApiKeysConfig = {
+	footballMatches: process.env.REACT_APP_API_KEY_FOOTBALL_MATCHES,
+	news: process.env.REACT_APP_API_KEY_NEWS,
+};
